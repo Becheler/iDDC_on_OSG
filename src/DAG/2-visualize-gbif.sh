c5b7548b4475fa4f97a2277ac198d18c9da333be
Arnaud Becheler <arnaud.becheler@gmail.com>
2022-03-01 11:26:17 -0500

    fix: chelsa stack output name

--- src/DAG/2-visualize-gbif.sh
+++ src/DAG/2-visualize-gbif.sh
@@ -4,10 +4,9 @@ python3 -m crumbs.get_chelsa \
       --points sampling-points.shp \
       --variables "dem" \
       --timesID "20" \
-      --margin 2.0 \
-      --geotiff dem.tif
+      --margin 2.0
 
-python3 -m crumbs.animate dem.tif \
+python3 -m crumbs.animate chelsa_stack_dem.tif \
       --gbif 1-gbif-occurrences/occurrences.shp \
       --no-DDD \
       --output occurrences.gif
