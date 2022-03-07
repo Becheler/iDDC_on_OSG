e03209eb0b7ba5d9154c8c6bd04d298a2a0b3062
Arnaud Becheler <arnaud.becheler@gmail.com>
2022-03-07 17:22:11 -0500

    fix: timesID argument

--- src/DAG/3-get-chelsa.sh
+++ src/DAG/3-get-chelsa.sh
@@ -3,7 +3,7 @@
 python3 -m crumbs.get_chelsa \
       --points sampling-points.shp \
       --variables dem,bio \
-      --timesID $t \
+      --timesID $1 \
       --margin 2.0 \
       --clip_dir 3-get-chelsa \
       --cleanup
