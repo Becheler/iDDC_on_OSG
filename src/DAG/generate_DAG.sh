3ca53e71f407dfdad148ca68943fe0b8f762a886
Arnaud Becheler <arnaud.becheler@gmail.com>
2022-03-14 15:05:29 -0400

    bug: join with iFS

--- src/DAG/generate_DAG.sh
+++ src/DAG/generate_DAG.sh
@@ -56,7 +56,9 @@ do
 done
 
 echo "JOB  SDM ../src/DAG/4-sdm.condor"
-echo "VARS SDM timesID=\"${timesID// /,}\""
+
+comma_separated_timesID=$(IFS=, ; echo "${timesID[*]}")
+echo "VARS SDM timesID=${comma_separated_timesID[@]}"
 
 for i in $(seq "$nb_sim")
 do
