9ce91009d6e340c3ccecd1f91ff4918ab582f4fd
Arnaud Becheler <arnaud.becheler@gmail.com>
2022-04-03 18:26:28 -0400

    fix: error in syntax

--- src/DAG/1-get-gbif.sh
+++ src/DAG/1-get-gbif.sh
@@ -7,9 +7,8 @@ export MPLCONFIGDIR
 python3 -m crumbs.get_gbif \
       --species "Heteronotia binoei" \
       --points sampling-points.shp \
-      #--limit 30 \
       --all \
-      #--year "1950,2022" \
+      --year "1900,2022" \
       --margin 2.0 \
       --output occurrences.shp
 
